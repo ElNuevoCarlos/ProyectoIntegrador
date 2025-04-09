a8cbf88763e6d82443ee8a310ff6aa118805e6eb
YesidBill <135476389+YesidBill@users.noreply.github.com>
2025-04-08 23:07:30 -0500

    Actualizacion de la interfaz de NewAccount

--- src/controllers/NewAccountController.java
+++ src/controllers/NewAccountController.java
@@ -1,14 +1,28 @@
 package controllers;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class NewAccountController {
 	@FXML private BorderPane rootPane;
 	@FXML private Button again;
+	@FXML
+	private ComboBox<String> TI;
+	
+	@FXML
+	public void initialize() {
+	    ObservableList<String> items = FXCollections.observableArrayList(
+	        "item-1", "item-2", "item-3", "item-4", "item-5"
+	    );
+	    TI.setItems(items);
+	}
+
 	
 	@FXML void handleName() {
 		
