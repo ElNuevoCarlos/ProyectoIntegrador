44e3dcb2caaf1b16f932769c9cb95923db09b15d
YesidBill <blizcano777@gmail.com>
2025-05-14 00:14:59 -0500

    Actualizacion de la interfaz de TeacherAdmin y su controller

--- src/controllers/MenuTeacherController.java
+++ src/controllers/MenuTeacherController.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import data.DataBase;
 import data.SessionManager;
 import data.UserDAO;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -13,8 +14,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -22,25 +27,87 @@ import model.User;
 
 public class MenuTeacherController {
     @FXML private BorderPane rootPane;
+    @FXML private Pane pane;
+    @FXML private GridPane grip;
+    @FXML private ImageView userImage, myLoansImage, requestConsultImage, closeSession;
     @FXML private  Hyperlink editUser;
-    @FXML private Text nameText, numIdentificationText, tiText, emailText, phoneText, roleText, pro_depText, password1Text, password2Text, title;
+    @FXML private Text myLoans, requestConsult, nameText, numIdentificationText, tiText, emailText, phoneText, roleText, pro_depText, password1Text, password2Text, title;;
+    @FXML private Separator separator1, separator2; 
     @FXML private TextField name, numIdentification, email, phone, pro_dep, password1, password2;
     @FXML private ComboBox<String> ti, role;
     @FXML private Button save;
     private Connection database = DataBase.getInstance().getConnection();
     private UserDAO userDao = new UserDAO(database);
 	private SessionManager sessionManager = SessionManager.getInstance();
+	
+	
+
     @FXML 
     public void initialize() {
-    	String[] partes = SessionManager.getInstance().getName().trim().split("\\s+");
-    	Rectangle rect = new Rectangle();
-    	rect.setX(0);                  
-    	rect.setY(0);                  
-    	rect.setWidth(150);           
-    	rect.setHeight(500);           
-    	rect.setFill(Color.BLUE); 
-    	rootPane.getChildren().add(0, rect);
-    	editUser.setText(partes[0] + " " + partes[1] + "\n" + SessionManager.getInstance().getRole());
+
+        String[] partes = SessionManager.getInstance().getName().trim().split("\\s+");
+        editUser.setText(partes[0] + " " + partes[1] + "\n" + SessionManager.getInstance().getRole());
+        editUser.setStyle("-fx-text-fill: #0f0f0f;");
+
+        // Usamos Platform.runLater para centrar los elementos una vez que la UI esté cargada
+        Platform.runLater(() -> {
+            double actualWidth = pane.getWidth();
+            double actualHeight = pane.getHeight();
+            double ySeparator1 = actualHeight / 3;
+            double ySeparator2 = (actualHeight / 3)*2;
+            separator1.setLayoutY(ySeparator1);
+            separator2.setLayoutY(ySeparator2);
+            userImage.setLayoutX((actualWidth - userImage.getBoundsInParent().getWidth()) / 2);
+            myLoansImage.setLayoutX((actualWidth - myLoansImage.getBoundsInParent().getWidth()) / 2);
+            requestConsultImage.setLayoutX((actualWidth - requestConsultImage.getBoundsInParent().getWidth()) / 2);
+            editUser.setLayoutX((actualWidth - editUser.getWidth()) / 2);
+            myLoans.setLayoutX((actualWidth - myLoans.getBoundsInParent().getWidth()) / 2);
+            requestConsult.setLayoutX((actualWidth - requestConsult.getBoundsInParent().getWidth()) / 2);
+            
+
+            // Centrar la imagen en el primer tercio
+            userImage.setLayoutY((ySeparator1 - userImage.getFitHeight()) / 2);
+            editUser.setLayoutY(userImage.getLayoutY() + userImage.getFitHeight());
+            myLoansImage.setLayoutY(((ySeparator1 + ySeparator2) - myLoansImage.getFitHeight()) / 2);
+            requestConsultImage.setLayoutY(((ySeparator2 + actualHeight) - requestConsultImage.getFitHeight()) / 2);
+            myLoans.setLayoutY(myLoansImage.getLayoutY() + myLoansImage.getFitHeight()+10);
+            requestConsult.setLayoutY(requestConsultImage.getLayoutY() + requestConsultImage.getFitHeight()+10);
+
+
+        });
+
+        // Listener para cuando cambie el ancho del pane (por redimensionamiento de la ventana, etc.)
+        pane.widthProperty().addListener((obs, oldVal, newVal) -> {
+            userImage.setLayoutX((newVal.doubleValue() - userImage.getFitWidth()) / 2);
+            editUser.setLayoutX((newVal.doubleValue() - editUser.getWidth()) / 2);
+            myLoansImage.setLayoutX((newVal.doubleValue() - myLoansImage.getFitWidth()) / 2);
+            requestConsultImage.setLayoutX((newVal.doubleValue() - requestConsultImage.getFitWidth()) / 2);
+            myLoans.setLayoutX((newVal.doubleValue() - myLoans.getBoundsInParent().getWidth()) / 2);
+            requestConsult.setLayoutX((newVal.doubleValue() - requestConsult.getBoundsInParent().getWidth()) / 2);
+            separator1.setPrefWidth(newVal.doubleValue());
+            separator2.setPrefWidth(newVal.doubleValue());
+        });
+
+
+        pane.heightProperty().addListener((obs, oldVal, newVal) -> {
+            double ySeparator1 = newVal.doubleValue() / 3;
+            double ySeparator2 = ySeparator1 * 2;
+
+            separator1.setLayoutY(ySeparator1);
+            separator2.setLayoutY(ySeparator2);
+
+            // Centrar verticalmente la imagen en el primer tercio
+            userImage.setLayoutY((ySeparator1 - userImage.getFitHeight()) / 2);
+            editUser.setLayoutY(userImage.getLayoutY() + userImage.getFitHeight());
+            
+            myLoansImage.setLayoutY(((ySeparator1 + ySeparator2) - myLoansImage.getFitHeight()) / 2);
+            myLoans.setLayoutY(myLoansImage.getLayoutY() + myLoansImage.getFitHeight()+10);
+            
+            requestConsultImage.setLayoutY(((ySeparator2 + newVal.doubleValue()) - requestConsultImage.getFitHeight()) / 2);
+            requestConsult.setLayoutY(requestConsultImage.getLayoutY() + requestConsultImage.getFitHeight()+10);
+            
+            closeSession.setLayoutY(newVal.doubleValue() - closeSession.getFitHeight() - 6);
+        });   	
     }
     
     @FXML
@@ -90,90 +157,90 @@ public class MenuTeacherController {
     	password2.setText(otherData[2]);
     	pro_dep.setText(otherData[3]);
     }
-    
-    @FXML
-    void handleSave() {
-		String Name = name.getText().trim();
-		String TI = ti.getValue() != null ? ti.getValue().trim() : "";
-		String NumIdentification = numIdentification.getText().trim();
-		String Pro_dep = pro_dep.getText().trim();
-		String Phone = phone.getText().trim();
-		String Email = email.getText().trim();
-		String Role = role.getValue() != null ? role.getValue().trim() : "";
-		String Password1 = password1.getText().trim();
-		String Password2 = password2.getText().trim();
-		
-        if (Name.isEmpty() || 
-        		TI.isEmpty() || 
-        		NumIdentification.isEmpty() || 
-        		Pro_dep.isEmpty() || 
-        		Phone.isEmpty() || 
-        		Email.isEmpty() || 
-        		Role.isEmpty() || 
-        		Password1.isEmpty() || 
-        		Password2.isEmpty()){
-        	
-        	showAlert("Error", "Campos vacíos", "Por favor, complete todos los campos.");
-            return;
-        }
-        String regexEmail = "^[a-zA-Z0-9._%+-]+@udi\\.edu\\.co$";
-        if (!Email.matches(regexEmail)) {
-        	showAlert("Error", "Correo inválido", "El correo debe tener el formato usuario@udi.edu.co");
-        	return;
-        }
-        if (!NumIdentification.matches("\\d{6,10}")) {
-        	showAlert("Error", "Número inválido", "Debe ingresar entre 6 y 10 dígitos numéricos.");
-        	return;
-        }
-        if (!Phone.matches("\\d{10}")) {
-        	showAlert("Error", "Número inválido", "Debe ingresar exactamente 10 dígitos numéricos.");
-        	return;
-        }
-        if (Password1.equals(Password2)) {
-        	if (!Password1.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,20}$")) {
-                showAlert("Formato inválido", "Contraseña insegura", 
-                        "La contraseña debe tener:\n" +
-                        "- Entre 8 y 20 caracteres\n" +
-                        "- Al menos una letra mayúscula\n" +
-                        "- Al menos una letra minúscula\n" +
-                        "- Al menos un número\n" +
-                        "- Al menos un carácter especial (@#$%^&+=!)");
-                    return;
-        	} else {
-        		User newUser = new User(Name, NumIdentification, TI, Email, Pro_dep, Phone, "ACTIVA", Role, Password1, sessionManager.getId());
-        		sessionManager.setUser(sessionManager.getId(), Name, Role, Email);
-	            if (showConfirmation("Confirmation", "Do you want to update your personal information?")) {
-	            	userDao.update(newUser);
-	            	showAlert("Updated", "Updated information", "Your data has been updated successfully.");
-	            	String[] partesUptate = Name.split("\\s+");
-	            	editUser.setText(partesUptate[0] + " " + partesUptate[1] + "\n" + Role);
-	            } else {
-	            	showAlert("Canceled", "Update cancelled", "No changes made.");
-	            }
-        		
-        	}
-        } else {
-        	showAlert("Error", "Contraseñas no coinciden", "Las contraseñas ingresadas no son iguales. Por favor, verifíquelas.");
-        	return;
-        }
-    }
-    
-    private void showAlert(String title, String head, String message) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
-        alert.setHeaderText(head);
-        alert.setContentText(message);
-        alert.showAndWait();
-    }
-    
-    private boolean showConfirmation(String title, String message) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-
-        ButtonType result = alert.showAndWait().orElse(ButtonType.CANCEL);
-        return result == ButtonType.OK;
-    }
+//    
+//    @FXML
+//    void handleSave() {
+//		String Name = name.getText().trim();
+//		String TI = ti.getValue() != null ? ti.getValue().trim() : "";
+//		String NumIdentification = numIdentification.getText().trim();
+//		String Pro_dep = pro_dep.getText().trim();
+//		String Phone = phone.getText().trim();
+//		String Email = email.getText().trim();
+//		String Role = role.getValue() != null ? role.getValue().trim() : "";
+//		String Password1 = password1.getText().trim();
+//		String Password2 = password2.getText().trim();
+//		
+//        if (Name.isEmpty() || 
+//        		TI.isEmpty() || 
+//        		NumIdentification.isEmpty() || 
+//        		Pro_dep.isEmpty() || 
+//        		Phone.isEmpty() || 
+//        		Email.isEmpty() || 
+//        		Role.isEmpty() || 
+//        		Password1.isEmpty() || 
+//        		Password2.isEmpty()){
+//        	
+//        	showAlert("Error", "Campos vacíos", "Por favor, complete todos los campos.");
+//            return;
+//        }
+//        String regexEmail = "^[a-zA-Z0-9._%+-]+@udi\\.edu\\.co$";
+//        if (!Email.matches(regexEmail)) {
+//        	showAlert("Error", "Correo inválido", "El correo debe tener el formato usuario@udi.edu.co");
+//        	return;
+//        }
+//        if (!NumIdentification.matches("\\d{6,10}")) {
+//        	showAlert("Error", "Número inválido", "Debe ingresar entre 6 y 10 dígitos numéricos.");
+//        	return;
+//        }
+//        if (!Phone.matches("\\d{10}")) {
+//        	showAlert("Error", "Número inválido", "Debe ingresar exactamente 10 dígitos numéricos.");
+//        	return;
+//        }
+//        if (Password1.equals(Password2)) {
+//        	if (!Password1.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,20}$")) {
+//                showAlert("Formato inválido", "Contraseña insegura", 
+//                        "La contraseña debe tener:\n" +
+//                        "- Entre 8 y 20 caracteres\n" +
+//                        "- Al menos una letra mayúscula\n" +
+//                        "- Al menos una letra minúscula\n" +
+//                        "- Al menos un número\n" +
+//                        "- Al menos un carácter especial (@#$%^&+=!)");
+//                    return;
+//        	} else {
+//        		User newUser = new User(Name, NumIdentification, TI, Email, Pro_dep, Phone, "ACTIVA", Role, Password1, sessionManager.getId());
+//        		sessionManager.setUser(sessionManager.getId(), Name, Role, Email);
+//	            if (showConfirmation("Confirmation", "Do you want to update your personal information?")) {
+//	            	userDao.update(newUser);
+//	            	showAlert("Updated", "Updated information", "Your data has been updated successfully.");
+//	            	String[] partesUptate = Name.split("\\s+");
+//	            	editUser.setText(partesUptate[0] + " " + partesUptate[1] + "\n" + Role);
+//	            } else {
+//	            	showAlert("Canceled", "Update cancelled", "No changes made.");
+//	            }
+//        		
+//        	}
+//        } else {
+//        	showAlert("Error", "Contraseñas no coinciden", "Las contraseñas ingresadas no son iguales. Por favor, verifíquelas.");
+//        	return;
+//        }
+//    }
+//    
+//    private void showAlert(String title, String head, String message) {
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//        alert.setTitle(title);
+//        alert.setHeaderText(head);
+//        alert.setContentText(message);
+//        alert.showAndWait();
+//    }
+//    
+//    private boolean showConfirmation(String title, String message) {
+//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+//        alert.setTitle(title);
+//        alert.setHeaderText(null);
+//        alert.setContentText(message);
+//
+//        ButtonType result = alert.showAndWait().orElse(ButtonType.CANCEL);
+//        return result == ButtonType.OK;
+//    }
 }
 
