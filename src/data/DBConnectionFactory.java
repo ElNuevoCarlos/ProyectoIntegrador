b4a7eb4e7cc906cf68188f2884d422c319f89d8e
Carlos <199296364+jjear11@users.noreply.github.com>
2025-06-06 22:06:13 -0500

    Update DBConnectionFactory.java

--- src/data/DBConnectionFactory.java
+++ src/data/DBConnectionFactory.java
@@ -4,13 +4,13 @@ public class DBConnectionFactory {
 	public static DBConnection getConnectionByRole(String role) {
 		switch (role.toLowerCase()) {
 		case "SUPERENCARGADO":
-		return SuperManagerConnection.getInstance();
+			return SuperManagerConnection.getInstance();
 		case "ENCARGADO":
-		return ManagerConnection.getInstance();
+			return ManagerConnection.getInstance();
 		case "DOCENTE":
-		return TeachaerAdminConnection.getInstance();
+			return TeachaerAdminConnection.getInstance();
 		case "ADMINISTRATIVO":
-		return TeachaerAdminConnection.getInstance();
+			return TeachaerAdminConnection.getInstance();
 		default:
 		throw new IllegalArgumentException("Rol no válido: " + role);}}
 }
